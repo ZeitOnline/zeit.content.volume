a8ca756b45cb44a954b2e82a37c223993528907c
Knut Goetz <Knut.Goetz@zeit.de>
2017-04-19 16:05:53 +0200

    Publish all boxes of articles when publishing the whole volume

--- src/zeit/content/volume/browser/admin.py
+++ src/zeit/content/volume/browser/admin.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from zeit.cms.i18n import MessageFactory as _
 from zeit.cms.workflow.interfaces import IPublish
-
+import itertools
+import zeit.cms.admin.browser.admin
+import zeit.cms.interfaces
 import zeit.content.article.article
 import zeit.content.infobox.infobox
-import zeit.cms.admin.browser.admin
-import zope.formlib.form
 import zeit.solr.query
+import zope.formlib.form
 
 
 class VolumeAdminForm(zeit.cms.admin.browser.admin.EditFormCI):
@@ -17,6 +18,9 @@ class VolumeAdminForm(zeit.cms.admin.browser.admin.EditFormCI):
     """
 
     extra_actions = zope.formlib.form.Actions()
+    assets_to_publish = [zeit.content.portraitbox.interfaces.IPortraitbox,
+                         zeit.content.infobox.interfaces.IInfobox
+                         ]
 
     @property
     def actions(self):
@@ -26,22 +30,44 @@ class VolumeAdminForm(zeit.cms.admin.browser.admin.EditFormCI):
     @zope.formlib.form.action(_("Publish content of this volume"),
                               extra_actions)
     def publish_all(self, action, data):
+        """
+        Publish articles marked as urgent and their boxes.
+        """
         Q = zeit.solr.query
         additional_constraints = [
             Q.field('published', 'not-published'),
-            Q.or_(
-                # Only Articles marked as urgent and infoboxes should be
-                # published
-                Q.and_(
-                    Q.bool_field('urgent', True),
-                    Q.field_raw(
-                        'type',
-                        zeit.content.article.article.ArticleType.type)),
-                Q.field_raw('type',
-                            zeit.content.infobox.infobox.InfoboxType.type),
-            ),
-
+            Q.and_(
+                Q.bool_field('urgent', True),
+                Q.field_raw(
+                    'type',
+                    zeit.content.article.article.ArticleType.type)),
         ]
-        to_publish = self.context.all_content_via_solr(
+        articles_to_publish = self.context.all_content_via_solr(
             additional_query_contstraints=additional_constraints)
-        IPublish(self.context).publish_multiple(to_publish)
+        # Flatten the list of lists and remove duplicates
+        all_content_to_publish = list(set(itertools.chain.from_iterable(
+                [self._with_dependencies(article) for article in
+                    articles_to_publish])))
+        IPublish(self.context).publish_multiple(all_content_to_publish)
+
+    def _with_dependencies(self, content):
+        """
+        :param content: CMSContent which dependencies are looked up.
+        :return: [dep1, dep2,..., content]
+        """
+        with_dependencies = [
+            content
+            for content in zeit.cms.interfaces.ICMSContentIterable(
+                content)
+            if self.needs_publishing(content)
+            ]
+        with_dependencies.append(content)
+        return with_dependencies
+
+    def _needs_publishing(self, content):
+        # Dont publish content which is already published
+        if zeit.cms.workflow.interfaces.IPublishInfo(content).published:
+            return False
+        # content has to provide one of interfaces defined above
+        return any([interface.providedBy(content) for interface
+                    in self.assets_to_publish])
